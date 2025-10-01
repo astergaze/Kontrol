108855166e38cd47b79b25f00a03db2eebb8a121
Thiago Martinez <thiagomartinez960@gmail.com>
2025-10-01 14:21:13 -0300

    Add basic backend structure and db config file

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+// const Sequelize = require("sequelize");
+
+// const sequelize = new Sequelize('postgres', 'postgres', '1234', {
+//     host: 'localhost',
+//     dialect: 'postgres'
+// })
+// module.exports = sequelize
+/* Use the below code if using SQL Server, otherwise if using PSQL use the one from above  */
+const Sequelize = require('sequelize-msnodesqlv8');
+
+const sequelize = new Sequelize('KontrolTest', null, null, {
+  dialect: 'mssql',
+  dialectModulePath: 'sequelize-msnodesqlv8',
+  dialectOptions: {
+    options: {
+      trustedConnection: true
+    }
+  },
+  host: 'localhost'
+});
+
+module.exports = sequelize;
--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3001;
 app.post('/api/hello', (req, res) => {
-  res.status(200).json({ message: 'Hola desde el backend!' });
+    res.status(200).json({ message: 'Hola desde el backend!' });
 });
 
-app.listen(port, () => {
-  console.log(`Servidor backend corriendo en http://localhost:${port}`);
+app.listen(port, async () => {
+    sequelize.sync({ force: false })
+    console.log(`Servidor backend corriendo en http://localhost:${port}`);
 });
