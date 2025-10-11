76f0157c84f4f3a87acfb732aa31665afa5fdd5a
Ulises-Alvelo <luisjorgealvelo@gmail.com>
2025-10-10 21:35:05 -0300

    Header correction

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './css/Header.css';
-
+import { useNavigate } from 'react-router-dom';
 const Header = () => {
+  const navigate = useNavigate();
+      const handleChat = () =>{
+    navigate('/chat')
+  };
   return (
     <div className="mainPageheader">
       <header>
@@ -10,7 +14,7 @@ const Header = () => {
         </div>
         <div className="usuario">
           <div className="iconos-superior">
-            <span className="chat"><img src="https://i.imgur.com/mwR5HzO.png" alt="logo chat" /></span>
+            <span className="chat"><img src="https://i.imgur.com/mwR5HzO.png" alt="logo chat" onClick={handleChat}/></span>
             <span className="bell"><img src="https://i.imgur.com/S4nFOIE.png" alt="logo campana" /></span>
           </div>
           <div className="perfil">
