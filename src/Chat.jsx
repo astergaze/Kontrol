6f58481d3152667374fa79115a564de93a9cca0b
hoaxed <thiagomartinez960@gmail.com>
2025-10-11 16:42:19 -0300

    Fixing CSS

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './css/chat.css';
-
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./css/chat.css";
+import Header from "./Header";
 const Chat = () => {
   const navigate = useNavigate();
 
-  
   const Return = () => {
-    navigate('/main');
+    navigate("/main");
   };
 
   return (
-    <div className='mainCont'>
-     <button className='return' onClick={Return}>Volver</button>
-      <div className='chatCont'>
-        <div className='users'></div>
+    <>
+      <Header />
+      <div className="mainCont">
+        <button className="return" onClick={Return}>
+          Volver
+        </button>
+        <div className="chatCont">
+          <div className="users"></div>
 
-        <div className='chatInputArea'>
-         <img src="https://i.imgur.com/fYLdolN.png" alt="Agregar archivo" id='addFile'/>
-           <input type="text" name="chatInput" id="chatBar" placeholder='Write here'/>
-         <img src="https://i.imgur.com/442CuF9.png" alt="Enviar" id='sendMessage'/>
+          <div className="chatInputArea">
+            <img
+              src="https://i.imgur.com/fYLdolN.png"
+              alt="Agregar archivo"
+              id="addFile"
+            />
+            <input
+              type="text"
+              name="chatInput"
+              id="chatBar"
+              placeholder="Write here"
+            />
+            <img
+              src="https://i.imgur.com/442CuF9.png"
+              alt="Enviar"
+              id="sendMessage"
+            />
+          </div>
         </div>
-
       </div>
-    </div>
+    </>
   );
 };
 
--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,27 +1,10 @@
 import React from 'react';
 import './css/MainPage.css';
-
+import Header from './Header';
 const JobCreation = () => {
   return (
     <div className="mainPage">
-      <header>
-        <div className="logo">
-          <img src="https://i.imgur.com/2ToRgc7.png" alt="Logo Kingdom"/>
-        </div>
-        <div className="usuario">
-          <div className="iconos-superior">
-            <span className="chat"><img src="https://i.imgur.com/mwR5HzO.png" alt="logo chat" /></span>
-            <span className="bell"><img src="https://i.imgur.com/S4nFOIE.png" alt="logo campana" /></span>
-          </div>
-          <div className="perfil">
-            <span className="icono-user"><img src="https://i.imgur.com/ZBIlQwo.png" alt="logo usuario" /></span>
-            <div>
-              <p>UsuarioEjemplo</p>
-              <small>Operador</small>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
 
       <main>
         <section className="card grande">
