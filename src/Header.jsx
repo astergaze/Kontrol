576126bc17395ff603ba64600b14d188fb848b67
hoaxed <thiagomartinez960@gmail.com>
2025-10-29 21:33:23 -0300

    JWT logic to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,13 +2,14 @@ import React, { useState, useEffect, useRef } from "react";
 import "./css/Header.css";
 import UserOptionsMenu from "./subcomponents/UserOptionsMenu";
 import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 import NotificationMenu from "./subcomponents/NotificationMenu";
 
 const Header = () => {
   const navigate = useNavigate();
   const [isNotifMenuOpen, setIsNotifMenuOpen] = useState(false);
   const [isUserMenuOpen, setIsUserMenuOpen] = useState(false);
-
+  const [userData, setUserData] = useState(null)
   // Referencias para cada menu
   const notifMenuRef = useRef(null);
   const userMenuRef = useRef(null);
@@ -28,6 +29,19 @@ const Header = () => {
   };
 
   useEffect(() => {
+      const token = localStorage.getItem("token")
+      if(token) {
+        try {
+          const decoded = jwtDecode(token)
+          setUserData(decoded)
+        } catch (e){
+          console.log("Invalido o expiro");
+          localStorage.removeItem("token")
+          navigate("/")
+        }
+      } else{
+        navigate("/")
+      }
     const handleClickOutside = (event) => {
       // Cierra el menu de usuario si el clic es fuera de su ref
       if (userMenuRef.current && !userMenuRef.current.contains(event.target)) {
@@ -46,8 +60,16 @@ const Header = () => {
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [isUserMenuOpen, isNotifMenuOpen]); 
-
+  }, [isUserMenuOpen, isNotifMenuOpen, navigate]); 
+  if (!userData) {
+    return (
+      <div className="mainPageheader">
+        <header>
+          Cargando
+        </header>
+      </div>
+    );
+  }
   return (
     <div className="mainPageheader">
       <header>
@@ -85,8 +107,8 @@ const Header = () => {
                 <img src="https://i.imgur.com/ZBIlQwo.png" alt="logo usuario" />
               </span>
               <div>
-                <p>UsuarioEjemplo</p>
-                <small>Administrador</small>
+                <p>{userData.nombre} {userData.apellido}</p>
+                <small>{userData.isAdmin ? "Administrador" : "Operador"}</small>
               </div>
             </div>
             {isUserMenuOpen && <UserOptionsMenu />}
