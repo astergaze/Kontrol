2e57cc4f67ca452d6b06cf768fc73fdec93d3d33
Ulises-Alvelo <luisjorgealvelo@gmail.com>
2025-11-13 20:10:08 -0300

    Fixing employee icons

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -18,55 +18,43 @@ const MainPage = () => {
         <main className="contenedor-general-cards">
           <div className="contenedor-cards">
             
-            <section className="card preView">
-              <h2>
-                <span className="vista">
-                  Vista de trabajos.
-                  {" "}
-                  <img
-                    className="logo5"
-                    src="https://i.imgur.com/Ftc3seh.png"
-                    alt="logo vista de trabajos"
-                  />
-                </span>
-              </h2>
-              <p>
-                Vea cuáles son los trabajos priorizados según la fecha de entrega.
-              </p>
-            </section>
+<section className="card preView">
+    <h2>Vista de trabajos.</h2> 
+    <span className="vista">
+        <img
+            className="card-icon" 
+            src="https://i.imgur.com/Ftc3seh.png"
+            alt="logo vista de trabajos"
+        />
+    </span>
+    <p>Vea cuáles son los trabajos priorizados según la fecha de entrega.</p>
+</section>
 
-            <section className="card lista" onClick={handlePriceListUser}>
-              <h2>
-                <span className="lista">
-                  Lista de precios.
-                  <img
-                    className="logo4"
-                    src="https://i.imgur.com/tqKQT8j.png"
-                    alt="logo lista de precios"
-                  />
-                </span>
-              </h2>
-              <p>
-                Vea todos los precios actualizados de los productos (tipo de papel,
-                terminación y personalizaciones finales).
-              </p>
-            </section>
-            <section className="card materialrequest" onClick={RequestMaterials}>
-              <h2>
-                <span className="solicitud">
-                  Solicitud de materiales.
-                  {" "}
-                  <img
-                    className="logo3"
-                    src="https://i.imgur.com/DcYlKKD.png"
-                    alt="logo solicitud de materiales"
-                  />
-                </span>
-              </h2>
-              <p>
-                Solicita materiales que hagan falta para continuar con el trabajo.
-              </p>
-            </section>
+<section className="card lista" onClick={handlePriceListUser}>
+    <h2>Lista de precios.</h2>
+    <span className="lista">
+        <img
+            className="card-icon" 
+            src="https://i.imgur.com/tqKQT8j.png"
+            alt="logo lista de precios"
+        />
+    </span>
+    <p>Vea todos los precios actualizados de los productos...</p>
+</section>
+
+<section className="card materialrequest" onClick={RequestMaterials}>
+    <h2>Solicitud de materiales.</h2>
+    <span className="solicitud">
+        <img
+            className="card-icon" 
+            id="small-icon"
+            src="https://i.imgur.com/DcYlKKD.png"
+            alt="logo solicitud de materiales"
+        />
+    </span>
+    <p>Solicita materiales que hagan falta para continuar con el trabajo.</p>
+</section>
+
 
           </div>
         </main>
--- src/MainPageAdmin.jsx
+++ src/MainPageAdmin.jsx
@@ -32,63 +32,52 @@ const MainPageAdmin = () => {
       <div className="mainPageAdmin">
         <main className="contenedor-general-cards">
           <div className="contenedor-cards">
-            <section className="card jobcreation" onClick={handleJobCreation}>
-              <h2>
-                <span className="entrada">
-                  Entrada del trabajo.
-                  {" "}
-                  <img
-                    className="logo1"
-                    src="https://i.imgur.com/CTGB4c8.png"
-                    alt="logo entrada de trabajo"
-                  />
-                </span>
-              </h2>
-              <p>Cliente, cotización, generación de orden de trabajo.</p>
-            </section>
+          <section className="card jobcreation" onClick={handleJobCreation}>
+            <h2>Entrada del trabajo.</h2>
+            <span className="entrada">
+              <img
+                className="card-icon" 
+                src="https://i.imgur.com/CTGB4c8.png"
+                alt="logo entrada de trabajo"
+              />
+            </span>
+            <p>Cliente, cotización, generación de orden de trabajo.</p>
+          </section>
 
-            <section className="card lista" onClick={handlePriceListAdmin}>
-              <h2>
-                <span className="lista">
-                  Lista de precios.
-                  {" "}
-                  <img
-                    className="logo4"
-                    src="https://i.imgur.com/tqKQT8j.png"
-                    alt="logo lista de precios"
-                  />
-
-                </span>
-              </h2>
-              <p>Precios actualizados de los productos.</p>
-            </section>
+   
+          <section className="card lista" onClick={handlePriceListAdmin}>
+            <h2>Lista de precios.</h2>
+            <span className="lista">
+              <img
+                className="card-icon"
+                src="https://i.imgur.com/tqKQT8j.png"
+                alt="logo lista de precios"
+              />
+            </span>
+            <p>Precios actualizados de los productos.</p>
+          </section>
             <section className="card" onClick={handleQuotation}>
-              <h2>
+              <h2>Generar cotizacion.</h2>
                 <span className="cotizacion">
-                  Generar cotización.
-                  {" "}
                   <img
                     className="logo2"
                     src="https://i.imgur.com/TYS7bE4.png"
                     alt="logo generar cotizacion"
                   />
                 </span>
-              </h2>
+              
               <p>Generación de PDF de cotización.</p>
             </section>
 
             <section className="card preView" onClick={handlePreViewJob}>
-              <h2>
+              <h2>Vista de trabajos.</h2>
                 <span className="vista">
-                  Vista de trabajos.
-                  {" "}
                   <img
                     className="logo5"
                     src="https://i.imgur.com/Ftc3seh.png"
                     alt="logo vista de trabajos"
                   />
                 </span>
-              </h2>
               <p>
                 Vea cuáles son los trabajos
                 <br />
@@ -100,17 +89,14 @@ const MainPageAdmin = () => {
               className="card materialrequest"
               onClick={handleMaterialRequest}
             >
-              <h2>
+              <h2> Ver solicitudes de materiales. </h2>
                 <span className="solicitud">
-                  Ver solicitudes de materiales.
-                  {" "}
                   <img
                     className="logo3"
                     src="https://i.imgur.com/DcYlKKD.png"
                     alt="logo ver solicitudes de materiales"
                   />
                 </span>
-              </h2>
               <p>
                 Ve las solicitudes de materiales que fueron hechas por los
                 operadores.
@@ -118,18 +104,14 @@ const MainPageAdmin = () => {
             </section>
 
             <section className="card signup" onClick={handleSignUp}>
-              <h2>
+              <h2> Alta de Operadores.</h2>
                 <span className="azul">
-                  Alta de Operadores.
-                  {" "}
                   <img
                     className="logo6"
                     src="https://i.imgur.com/fjRwV7z.png"
                     alt="logo alta de operadores"
                   />
-
                 </span>
-              </h2>
               <p>Da de alta a los nuevos usuarios que ingresen a Kingdom.</p>
             </section>
           </div>
